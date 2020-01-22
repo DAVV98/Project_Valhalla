40ef184b5b5702696d90b6d74ed857833d44dd21
Dan <danielvillatepsn@gmail.com>
2020-01-22 14:52:03 +0000

    Started AI of Ranged Enemy
    - Moves back and forth like intended.
    
    TODO:
    - Make it so its not jagged at minDist.

--- Project_Valhalla_Alpha/Assets/Scripts/Enemies/Axe Thrower/AI_Range.cs
+++ Project_Valhalla_Alpha/Assets/Scripts/Enemies/Axe Thrower/AI_Range.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class AI_Range : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Target")]
+    public Transform Player;
+
+    [Header("Characteristics")]
+    public int moveSpeed;
+    public int maxDist;
+    public int minDist;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Range_AI();
+    }
+
+    void Range_AI()
+    {
+        transform.LookAt(Player);
+
+        if(Vector3.Distance(transform.position, Player.position) > minDist)
+        {
+            transform.position += transform.forward * moveSpeed * Time.fixedDeltaTime;
+        }
+        else if (Vector3.Distance(transform.position, Player.position) < maxDist)
+        {
+            transform.position -= transform.forward * moveSpeed * Time.fixedDeltaTime;
+        }
+
+        //TODO make it so that enemy isnt jagged when at minDist.
     }
 }
