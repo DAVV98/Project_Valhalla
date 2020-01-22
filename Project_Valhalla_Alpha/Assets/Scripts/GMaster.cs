63220f336b18f70928e446c6d25aaec7baf882d8
jlaassri <jordanlaassri@hotmail.com>
2020-01-22 13:25:13 +0000

    pickup & master added
    added root Source and pick up for health
    
    https://www.youtube.com/watch?v=VzN0dfq8Gto

---
+++ Project_Valhalla_Alpha/Assets/Scripts/GMaster.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GMaster : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int health = 4;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int keys = 0;
+        
+
+        
+    }
+}
