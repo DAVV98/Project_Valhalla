de44b1b0a3430003f345c558935c3b826877ae63
Dan <danielvillatepsn@gmail.com>
2020-01-23 13:48:20 +0000

    Adding axe throw.
    - Bug: timer not making axe throw.

--- Project_Valhalla_Alpha/Assets/Scripts/Enemies/Axe Thrower/AI_Range.cs
+++ Project_Valhalla_Alpha/Assets/Scripts/Enemies/Axe Thrower/AI_Range.cs
@@ -9,15 +9,34 @@ public class AI_Range : MonoBehaviour
 
     [Header("Characteristics")]
     public int moveSpeed;
-    public int maxDist;
-    public int minDist;
+    public float maxDist;
+    public float minDist;
+
+    [Header("Axe Throw")]
+    public GameObject axeObject;
+    public Transform axeSpawn;
+    float throwTimer;
+    public float throwSpeed;
+    public float throw_every;
+
+    void Start()
+    {
+        throwTimer = 0;
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        throwTimer += Time.deltaTime;
         Range_AI();
+        
     }
 
+    /// <summary>
+    /// - Makes enemy look at player.
+    /// - Creates the movement AI for axeThrower. 
+    /// - TODO: make axe throw ability.
+    /// </summary>
     void Range_AI()
     {
         transform.LookAt(Player);
@@ -32,7 +51,31 @@ public class AI_Range : MonoBehaviour
         {
             transform.position -= transform.forward * moveSpeed * Time.fixedDeltaTime;
         }
-        
-        //TODO make it so that enemy isnt jagged when at minDist.
+
+        if (Vector3.Distance(transform.position, Player.position) < maxDist)
+        {
+            AttackTimer();
+        }
+    }
+
+    void AttackTimer()
+    {
+        //Timer x time.
+        if (throwTimer == throw_every)
+        {
+            Debug.Log("shoot");
+        }
+    }
+
+    void Attack()
+    {
+        //instantiate new axe.
+        GameObject newAxe = Instantiate(axeObject, axeSpawn.position, axeSpawn.rotation);
+
+        //throw axe
+        newAxe.GetComponent<Rigidbody>().velocity = axeSpawn.forward * throwSpeed;
+
     }
 }
+
+//call attack once every 5 seconds.
