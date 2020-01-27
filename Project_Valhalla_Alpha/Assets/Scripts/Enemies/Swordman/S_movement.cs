c7eb2af292ea9380f5f57e8add3e96ebcdcd469d
jlaassri <jordanlaassri@hotmail.com>
2020-01-27 14:22:09 +0000

    setting a dist

--- Project_Valhalla_Alpha/Assets/Scripts/Enemies/Swordman/S_movement.cs
+++ Project_Valhalla_Alpha/Assets/Scripts/Enemies/Swordman/S_movement.cs
@@ -6,19 +6,29 @@ public class S_movement : MonoBehaviour
 {
     [SerializeField] Transform target;
     [SerializeField] float MovmentSpeed = 10f;
-    [SerializeField] float rotationalDamp = 0.5f;
+    
+
+    public float Dist;
+    
 
     void Update()
     {
-        turn();
-        Move();
+        if (Vector3.Distance(transform.position, target.position) < Dist)
+        {
+            turn();
+            Move();
+        }
+        
     }
 
     void turn()
     {
-        Vector3 pos = target.position - transform.position;
-        Quaternion rotation = Quaternion.LookRotation(pos);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationalDamp * Time.deltaTime);
+
+        transform.LookAt(target);
+
+        // Vector3 pos = target.position - transform.position;
+        // Quaternion rotation = Quaternion.LookRotation(pos);
+        // transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationalDamp * Time.deltaTime);
 
     }
 
