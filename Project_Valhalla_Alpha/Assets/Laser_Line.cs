422c7e0667bb8c18d763098b9eb37303b0d3e2f2
Dan <danielvillatepsn@gmail.com>
2020-01-29 15:47:30 +0000

    Added laser trap
    TODO
    
    - Triggers.
    - Kill player.

---
+++ Project_Valhalla_Alpha/Assets/Laser_Line.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser_Line : MonoBehaviour
+{
+    private LineRenderer lr;
+    public Transform startPos;
+    public Transform endPos;
+   
+    // Use this for initialization
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //set laser start pos.
+
+        lr.SetPosition(0, startPos.position);
+        lr.SetPosition(1, endPos.position);
+    }
+}
