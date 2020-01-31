711d324ed6fef79746a6f5cec6c7bac44b4810dd
Dan <danielvillatepsn@gmail.com>
2020-01-31 01:15:25 +0000

    fixed aim, added push

--- Project_Valhalla_Alpha/Assets/Scripts/Player/Player_Move_Aim.cs
+++ Project_Valhalla_Alpha/Assets/Scripts/Player/Player_Move_Aim.cs
@@ -7,6 +7,10 @@ public class Player_Move_Aim : MonoBehaviour
     [Header("Movement")]
     public float moveSpeed;
 
+    [Header("Aim")]
+    public float rotateSpeed;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,14 +22,15 @@ public class Player_Move_Aim : MonoBehaviour
     {
         movePlayer();
         mouseAim();
+        //arrowsAim();
     }
 
 
     void movePlayer()
     {
         //Get Axis
-        float input_horizontal = Input.GetAxisRaw("Horizontal");
-        float input_vertical = Input.GetAxisRaw("Vertical");
+        float input_horizontal = Input.GetAxisRaw("Horizontal_WASD");
+        float input_vertical = Input.GetAxisRaw("Vertical_WASD");
 
         //Create x,y,z movement.
         float x = input_horizontal * moveSpeed * Time.fixedDeltaTime;
@@ -41,17 +46,53 @@ public class Player_Move_Aim : MonoBehaviour
 
     void mouseAim()
     {
-        //Position of Object
-        Vector2 screenPos = Camera.main.WorldToViewportPoint(transform.position);
+        /*
+          gamesplusjames (2016). Available at: https://www.youtube.com/watch?v=lkDGk3TjsIE (Accessed: 31 Jan 2020).
+        */
 
-        //Position of Mouse
-        Vector2 mousePos = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition + Vector3.back * 10f);
+        Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane ground = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
 
-        //Object -> Mouse Angle
-        float angle = AngleBetweenTwoPoints(screenPos, mousePos);
+        if(ground.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            Debug.DrawLine(cameraRay.origin, pointToLook, Color.blue);
 
-        //Rotate player
-        transform.rotation = Quaternion.Euler(new Vector3(0f, -angle, 0f));
+            transform.LookAt(new Vector3(pointToLook.x, this.transform.position.y, pointToLook.z));
+        }
+        
+
+      
+    }
+
+    void arrowsAim()
+    {
+        /*
+        //get Axis
+    
+        float yRotation = Input.GetAxisRaw("Horizontal_Arrows");
+
+        //create x,y,z rotation
+        float yRotate = yRotation * rotateSpeed * Time.fixedDeltaTime;
+
+        //rotate vector
+        Vector3 rotate_player = new Vector3(0, yRotate, 0);
+
+        //aim player
+        transform.Rotate(rotate_player);
+        */
+
+        /*
+        Vector3 movementDirection = new Vector3(Input.GetAxis("Horizontal_Arrows"), 0, Input.GetAxis("Vertical_Arrows"));
+        Vector3 realDirection = Camera.main.transform.TransformDirection(movementDirection);
+        // this line checks whether the player is making inputs.
+        if (realDirection.magnitude > 0.1f)
+        {
+            Quaternion newRotation = Quaternion.LookRotation(realDirection);
+            transform.rotation = newRotation;
+        }
+        */
     }
 
     float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
---
+++ Project_Valhalla_Alpha/Assets/Scripts/Player/Shield_Push.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield_Push : MonoBehaviour
+{
+    [Header("Shield_Push")]
+    public float SP_Range;
+    public float SP_Force;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ShieldPush();
+        }
+    }
+
+    void ShieldPush()
+    {
+        //creates layermask to ignore player objects.
+        int layerMask = 1 << 8;
+        layerMask = ~layerMask;
+
+        //sends ray forward.
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+        RaycastHit shield_hit;
+        if (Physics.Raycast(transform.position, fwd, out shield_hit, SP_Range, layerMask))
+        {
+            if (shield_hit.collider.tag == "Enemy" || shield_hit.collider.tag == "Pushable")
+            {
+                //Destroy(shield_hit.rigidbody.gameObject);
+
+                shield_hit.rigidbody.AddForceAtPosition(SP_Force * fwd, shield_hit.point);
+            }
+        }
+
+    }
+}
