428e52d7dc3445948be186db8a0621acb570662d
Will Reynard <wjreynard@outlook.com>
2020-02-02 11:07:55 +0000

    reorganised folders

---
+++ Project_Valhalla_Alpha/Assets/Arrow.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    public float lifetime = 10.0f;
+    private float age = 0.0f;
+    public float arrowSpeed = 6.0f;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // update age
+        age += 0.1f;
+        if (age > 10.0f) // lifetime doesn't work for some reason? because it was initialised to 3.0f at first?
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // look in direction of movement
+        transform.LookAt(transform.position + rb.velocity);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Shield"))
+        {
+            ReflectArrowByRaycast(other);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Shield"))
+        {
+            ReflectArrowByCollision(collision);
+        }
+    }
+
+    private void ReflectArrowByCollision(Collision collision)
+    {
+        // code from: https://answers.unity.com/questions/352609/how-can-i-reflect-a-projectile.html
+        Debug.Log("ReflectArrowByCollision() called");
+        ContactPoint contact = collision.contacts[0];
+        Vector3 oldVelocity = rb.velocity;
+        Vector3 reflectedVelocity = Vector3.Reflect(oldVelocity, contact.normal);
+        rb.velocity = reflectedVelocity;
+        Quaternion rotation = Quaternion.FromToRotation(oldVelocity, reflectedVelocity);
+        transform.rotation = rotation * transform.rotation;
+        // end of code block
+    }
+
+    private void ReflectArrowByRaycast(Collider other)
+    {
+        Debug.Log("ReflectArrow() called");
+
+        // stop shield in place
+        other.attachedRigidbody.velocity = Vector3.zero;
+
+        // reset age so arrow persists longer
+        age = 0.0f;
+
+        // user raycast to access vector normal for reflection
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, rb.velocity);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Vector3 direction = Vector3.Reflect(rb.velocity, hit.normal);
+            rb.velocity = direction.normalized * arrowSpeed;
+            
+            Debug.Log("ReflectArrow() direction = " + direction);
+        }
+    }
+}
---
+++ Project_Valhalla_Alpha/Assets/ArrowTrap.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowTrap : MonoBehaviour
+{
+    public bool bActive = false;
+    public GameObject arrowPrefab;
+    private int shootTimer = 0;
+    private int shootInterval = 50;
+    public bool bMultiDirections = true;
+
+    public float arrowSpeed = 6.0f;
+
+    private void Update()
+    {
+
+        // shrink size
+        if (bActive)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else
+        {
+            transform.localScale = Vector3.one * 0.75f;
+        }
+    }
+
+    // improvements
+    //  1. could use object pooling for better performance
+    private void FixedUpdate()
+    {
+        if (bActive)
+        {
+            shootTimer++;
+
+            if (shootTimer % shootInterval == 0)
+            {
+                if (bMultiDirections)
+                {
+                    //  2. could use for loop?
+                    GameObject newArrow1 = Instantiate(arrowPrefab, gameObject.transform.position, Quaternion.identity);
+                    newArrow1.GetComponent<Rigidbody>().velocity = gameObject.transform.right * arrowSpeed;
+
+                    GameObject newArrow2 = Instantiate(arrowPrefab, gameObject.transform.position, Quaternion.identity);
+                    newArrow2.GetComponent<Rigidbody>().velocity = gameObject.transform.right * arrowSpeed * -1.0f;
+
+                    GameObject newArrow3 = Instantiate(arrowPrefab, gameObject.transform.position, Quaternion.identity);
+                    newArrow3.GetComponent<Rigidbody>().velocity = gameObject.transform.forward * arrowSpeed;
+
+                    GameObject newArrow4 = Instantiate(arrowPrefab, gameObject.transform.position, Quaternion.identity);
+                    newArrow4.GetComponent<Rigidbody>().velocity = gameObject.transform.forward * arrowSpeed * -1.0f;
+                }
+                else
+                {
+                    GameObject newArrow = Instantiate(arrowPrefab, gameObject.transform.position, gameObject.transform.rotation);
+                    newArrow.GetComponent<Rigidbody>().velocity = gameObject.transform.forward * arrowSpeed;
+                }
+                shootTimer = 0;
+            }
+        }
+    }
+}
