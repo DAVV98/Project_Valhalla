584b7bd0cbed7357e00d4a4fbf9a9c4ac3547b19
Will Reynard <wjreynard@outlook.com>
2020-02-03 16:38:38 +0000

    added ShieldPushableBlock prefab

---
+++ Project_Valhalla_Alpha/Assets/Scripts/ShieldPushableBlock.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldPushableBlock : MonoBehaviour
+{
+    public float minHeight = -4.0f;
+
+    private void Update()
+    {
+        if (transform.position.y <= minHeight)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
