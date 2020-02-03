1ac07739ff73462a83a197cd5cd66db70076d565
jlaassri <jordanlaassri@hotmail.com>
2020-02-03 16:48:10 +0000

    pick up object fixes better

---
+++ Project_Valhalla_Alpha/Assets/Pickup.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public Transform TheDest;
+
+    public Transform player;
+
+    public float Dist;
+
+
+    void Update()
+    {
+
+
+        if (Input.GetKey(KeyCode.E) && (Vector3.Distance(transform.position, player.position) < Dist))
+        {
+            this.transform.position = TheDest.position;
+            this.transform.parent = GameObject.Find("Dest").transform;
+        }
+
+        else
+        {
+            this.transform.parent = null;
+        }
+        
+
+    }
+
+    
+
+
+
+}
