de0348b6fabceed5f71a42920f92d413dcaffb6f
Will Reynard <wjreynard@outlook.com>
2020-02-13 18:07:55 +0000

    Update CameraController.cs

--- Project_Valhalla_Alpha/Assets/Scripts/CameraController.cs
+++ Project_Valhalla_Alpha/Assets/Scripts/CameraController.cs
@@ -17,7 +17,7 @@ public class CameraController : MonoBehaviour
     // research on LateUpdate() or FixedUpdate():
     //  1. https://forum.unity.com/threads/solved-camera-jitter-as-soon-as-using-lerp.762116/
     //  2. https://starmanta.gitbooks.io/unitytipsredux/content/all-my-updates.html
-    private void LateUpdate() // LateUpdate() or FixedUpdate() if moving player via Rigidbody physics
+    private void FixedUpdate() // LateUpdate() or FixedUpdate() if moving player via Rigidbody physics
     {
         if (!player.GetComponent<Player_v3>().bPlayerFalling)
         {
