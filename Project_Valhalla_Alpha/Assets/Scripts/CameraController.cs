6c982768770d7fe709b8a909fffe239a0c10d26a
Will Reynard <wjreynard@outlook.com>
2020-03-09 16:23:50 +0000

    added new tag

--- Project_Valhalla_Alpha/Assets/Scripts/CameraController.cs
+++ Project_Valhalla_Alpha/Assets/Scripts/CameraController.cs
@@ -33,8 +33,18 @@ public class CameraController : MonoBehaviour
 
     private void MoveCamera()
     {
+        if (!player.GetComponent<Player_v3>().bPlayerFalling)
+        {
+            Vector3 desiredPosition = player.transform.position + offset;
+            // Vector3.Lerp() from borrowed from: https://www.youtube.com/watch?v=MFQhpwc6cKE
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+            transform.position = smoothedPosition;
 
-        if (!player.GetComponent<Player_v5>().bPlayerFalling  || !player.GetComponent<Player_v3>().bPlayerFalling)
+            // make the camera fixed on the player
+            //transform.LookAt(player.transform);
+        }
+
+        if (!player.GetComponent<Player_v5>().bPlayerFalling)
         {
             Vector3 desiredPosition = player.transform.position + offset;
             // Vector3.Lerp() from borrowed from: https://www.youtube.com/watch?v=MFQhpwc6cKE
