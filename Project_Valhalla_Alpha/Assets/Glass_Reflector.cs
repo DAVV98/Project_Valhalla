c6f4e63322c2353632d56a196146d90cff38d206
Dan <danielvillatepsn@gmail.com>
2020-03-18 13:03:08 +0000

    .

---
+++ Project_Valhalla_Alpha/Assets/Glass_Reflector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glass_Reflector : MonoBehaviour
+{
+    public GameObject child;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        child.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Laser")
+        {
+            child.SetActive(true);
+        }
+    }
+}
