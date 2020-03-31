f00c7a37394cabc020c772d67783e3e8d13eee2b
Dan <danielvillatepsn@gmail.com>
2020-03-31 12:15:03 +0100

    Bug fixes intro

--- Project_Valhalla_Alpha/Assets/Scripts/LaserKill.cs
+++ Project_Valhalla_Alpha/Assets/Scripts/LaserKill.cs
@@ -6,13 +6,18 @@ public class LaserKill : MonoBehaviour
 {
     private void OnParticleCollision(GameObject other)
     {
-        if (other.tag == "Player"||other.tag == "Axe Enemy")
+        if (other.tag == "Player")
         {
             other.GetComponent<Player_v5>().DamagePlayer(9);
         }
         else if (other.tag == "ProjectileTrigger")
         {
             other.GetComponent<ProjectileTrigger>().Active = true;
+        }
+
+        if( other.tag == "Axe Enemy")
+        {
+            Destroy(other.gameObject);
         }
     }
 }
