25387e9521f3e87bdb8580f552459a713bcf7227
Will Reynard <wjreynard@outlook.com>
2020-03-31 11:41:04 +0100

    playtested level 3, fixed turbine block pushing through walls, added fade to turbine block particles

--- Project_Valhalla_Alpha/Assets/Scripts/Turbine.cs
+++ Project_Valhalla_Alpha/Assets/Scripts/Turbine.cs
@@ -18,24 +18,24 @@ public class Turbine : MonoBehaviour
             }
         }
 
-        RaycastHit[] hits;
-        hits = Physics.RaycastAll(transform.position, transform.forward, 100.0f);
-        for (int i = 0; i < hits.Length; i++)
-        {
-            RaycastHit hit = hits[i];
+        //RaycastHit[] hits;
+        //hits = Physics.RaycastAll(transform.position, transform.forward, 100.0f);
+        //for (int i = 0; i < hits.Length; i++)
+        //{
+        //    RaycastHit hit = hits[i];
 
-            if (hit.transform.gameObject.layer == 8)
-            {
-                //Debug.Log("Turbine::FixedUpdate(), RaycastAll[] hit stone block");
-                return;
-            }
+        //    if (hit.transform.gameObject.layer == 8)
+        //    {
+        //        //Debug.Log("Turbine::FixedUpdate(), RaycastAll[] hit stone block");
+        //        return;
+        //    }
 
-            if (hit.collider.GetComponent<MoveByWind>()) {
-                //Debug.Log("Turbine::FixedUpdate(), RaycastAll[]");
-                windArea = hit.collider.gameObject;
-                windArea.GetComponent<Rigidbody>().AddForce(transform.forward * windForce);
-                //return;
-            }
-        }
+        //    if (hit.collider.GetComponent<MoveByWind>()) {
+        //        //Debug.Log("Turbine::FixedUpdate(), RaycastAll[]");
+        //        windArea = hit.collider.gameObject;
+        //        windArea.GetComponent<Rigidbody>().AddForce(transform.forward * windForce);
+        //        //return;
+        //    }
+        //}
     }
 }
